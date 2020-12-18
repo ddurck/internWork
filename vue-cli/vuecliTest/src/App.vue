1554e037394116bda4ffec4b2647c0b421a6a771
ddurck <longnina678@gmail.com>
2020-12-18 14:57:07 +0800

    init project

---
+++ vue-cli/vuecliTest/src/App.vue
@@ -0,0 +1,40 @@
+<template>
+  <div id="app">
+    <img src="./assets/logo.png">
+    <router-view/>
+    <p>导航 ：
+       <router-link to="/">首页</router-link>
+       <router-link to="/hi">Hi页面</router-link>
+       <!-- <router-link :to="{name:''hi, params:{username:'jjj'}}">Hi页面</router-link> -->
+       <!-- 通过这个name和组件匹配，而不是通过path。然后取参数的时候： -->
+       <!-- {{$route.params.username}} -->
+       <router-link to="/hi/hi1">Hi页面1</router-link>
+       <router-link to="/hi/hi2">Hi页面2</router-link>
+    </p>
+    <button @click="goback">后退</button>
+    <p>{{$route.name}}</p> // 接收路由传值
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'App',
+  methods: {
+    goback () {
+      this.$router.go(-1)
+    }
+  }
+}
+
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ vue-cli/vuecliTest/src/components/Hi.vue
@@ -0,0 +1,36 @@
+<template>
+  <div class="hello">
+    <h1>{{ msg }}</h1>
+    <h1>nihaonina</h1>
+    <router-view class="aaa"></router-view>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Hi',
+  data () {
+    return {
+      msg: 'hi nini'
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h1, h2 {
+  font-weight: normal;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #ff5050;
+}
+</style>
